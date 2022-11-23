4ebb86f5971bdf6a7e8ae50decebbbf85555a100
Administrator <heridonvi_tagaban12@yahoo.com>
2022-11-23 12:19:37 +0800

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  description = 'Hello World';
+  todos = [
+    {
+      id: 1,
+      description: 'Dancing',
+      isCompleted: false
+    },
+    {
+      id: 2,
+      description: 'Programming',
+      isCompleted: false
+    },
+    {
+      id: 3,
+      description: 'Basketball',
+      isCompleted: false
+    }
+  ]
+
+  ngOnInit() {
+  }
+
+  todosLength () {
+    return this.todos.length
+  }
+
+  addTodo () {
+    this.todos.push({
+      id: 1,
+      description: this.description,
+      isCompleted: false
+    })
+    this.description = ''
+    alert('Added!')
+  }
+
+  removeTodo (i: any) {
+    this.todos.splice(i, 1)
+    alert('Removed!')
+  } 
 }
